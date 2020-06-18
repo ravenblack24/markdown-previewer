1ab385b67de9761988d0e6d41bcf23aaa0d0f4d9
Your Name <you@example.com>
2020-06-18 12:57:59 +0100

    Style complete, 'Marked' install

--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from "./App";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWindowMaximize } from '@fortawesome/free-solid-svg-icons';
+import { faFreeCodeCamp } from '@fortawesome/free-brands-svg-icons';
+import marked from 'marked';
+
+class MarkDownPreviewer extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <section className="section--editor">
+          <header>
+            <FontAwesomeIcon icon={faFreeCodeCamp} className="icon" />
+            <h1>Editor</h1>
+            <FontAwesomeIcon icon={faWindowMaximize} className="icon icon-action" />
+          </header>
+    <textarea id="editor"></textarea>
+        </section>
+        <section className="section--preview">
+          <header>
+            <FontAwesomeIcon icon={faFreeCodeCamp} className="icon" />
+            <h1>Previewer</h1>
+            <FontAwesomeIcon icon={faWindowMaximize} className="icon icon-action" />
+          </header>
+          <div id="preview">{marked("# hello there")}</div>
+        </section>
+      </React.Fragment>
+    );
+  }
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MarkDownPreviewer />
   </React.StrictMode>,
   document.getElementById('root')
 );
