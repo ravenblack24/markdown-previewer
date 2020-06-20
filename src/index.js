7fd3f3bc71de21f4898ce06cb81284f9a2656e92
Your Name <you@example.com>
2020-06-20 17:42:46 +0100

    Complete, includes FCC tests, Helmet JS and updated default editor content

--- src/index.js
+++ src/index.js
@@ -5,13 +5,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWindowMaximize } from '@fortawesome/free-solid-svg-icons';
 import { faFreeCodeCamp } from '@fortawesome/free-brands-svg-icons';
 import marked from 'marked';
+import ReactFCCtest from 'react-fcctest';
+import {Helmet} from "react-helmet";
 
 const defaultInput = `# Welcome to my React Markdown Previewer!
 ## This is a subheading!
 
+Testing a line break
+here is the second line
+
 Here's an example of a [link](https://github.com/ravenblack24/markdown-previewer)
 
-This is some _inline_ sample code <code>\`<div>Hello World!</div>\`</code>
+This is some _inline_ sample code \`<div>Hello World!</div>\`
+
+\`\`\`
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == \"\`\`\`\" && lastLine == \"\`\`\`\") {
+    return multiLineCode;
+  }
+}
+\`\`\`
 
 How about some **list items** next:
 
@@ -77,14 +90,20 @@ class MarkDownPreviewer extends React.Component {
 
   createMarkup() {
     console.log(marked(this.state.input));
-    return{__html: marked(this.state.input)}
+    return{__html: marked(this.state.input, {breaks: true})}
   }
 
   render() {
     return (
       <React.Fragment>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>ReactJS - Markdown Previewer</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+	      </Helmet>
         <Panel component={<textarea id="editor" value={this.state.input} onChange={this.handleChange}></textarea>} title={"Editor"}/>
         <Panel component={<div id="preview" dangerouslySetInnerHTML={this.createMarkup()} />} title={"Previewer"}/>
+        <ReactFCCtest />
       </React.Fragment>
     );
   }
